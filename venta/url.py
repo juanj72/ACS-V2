4de2bd62aec6b40ebe84bc3eb9569c662f663db0
juan jose <juan.jara@campusucc.edu.co>
2023-06-30 11:02:29 -0500

    agregacion ventana de recibos

--- venta/url.py
+++ venta/url.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('eliminar/<id>/<reb>',eliminardetail,name='eliminardetalle'),
     path('cerrar/<id>',cerrarrecibo,name='cerrarRecibo'),
     path('anular/<id>',anular,name = 'anular'),
-    path('recibo_detalle/<id>',detalle_recibo,name='detalle_recibo')
+    path('recibo_detalle/<id>',detalle_recibo,name='detalle_recibo'),
+    path('listar/',listar,name='verventas')
 ]
  
--- venta/views.py
+++ venta/views.py
@@ -180,4 +180,58 @@ def detalle_recibo(request,id):
 
 
 def listar(request):
-    return render(request,'ventas/verventas.html')
+    recibos = recibos_lista()
+    return render(request,'ventas/verventas.html',{'recibos':recibos})
+
+
+def recibos_lista():
+     with connection.cursor() as cursor:  # Activamos un cursor para las consultas a la BD
+    
+        sql = """
+ SELECT 
+    re.id as numero_recibo,
+    
+    est.nombre as estado,
+    re.fecha as fecha_recibo,
+    concat(c.nombre,' ',c.apellido) as cliente,
+    c.documento as documento,
+  
+    sum(det.cantidad*pro.valor_publico) as 'total'
+    
+FROM
+    venta_recibo re
+        INNER JOIN
+    cliente c ON c.id = re.cliente_id
+        INNER JOIN
+    venta_detalle det ON det.recibo_id = re.id
+        INNER JOIN
+    venta_estado est ON est.id = re.estado_id
+    inner join producto pro on det.producto_id=pro.id
+    
+    group by re.id
+        """
+
+        # Ejecutar una linea SQL En este caso llamamos un procedimiento almacenado
+        cursor.execute(sql)
+        #total=total_fecha(fecha)
+
+        columns = []  # Para guardar el nombre de las columnas
+
+        # Recorrer la descripcion (Nombre de la columna)
+        for column in cursor.description:
+
+            columns.append(column[0])  # Guardando el nombre de las columnas
+
+        data = []  # Lista con los datos que vamos a enviar en JSON
+
+        for row in cursor.fetchall():  # Recorremos las fila guardados de la BD
+
+            # Insertamos en data un diccionario
+            data.append(dict(zip(columns, row)))
+
+        cursor.close()  # Se cierra el cursor para que se ejecute el procedimiento almacenado
+
+        connection.commit()  # Enviamos la sentencia a la BD
+        connection.close()
+
+        return(data)
